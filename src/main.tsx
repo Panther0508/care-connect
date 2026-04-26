029b779d19d8190f08ca6d8000a1ffb37560598a
Panther0508 <ngbaronyeoswald@gmail.com>
2026-04-26 11:04:21 +0100

    wrap App in ErrorBoundary

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,13 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
 import "./index.css";
+import { ErrorBoundary } from "./components/ErrorBoundary";
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
-    <App />
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
   </BrowserRouter>
 );
 
